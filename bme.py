57ddcb901fa5b27dec99a59c4a87059fb779378e
maxsorto <maxsorto@gmail.com>
2016-05-05 04:46:53 -0400

    Adding initial fileswhich include BME280 drivers and script

---
+++ bme.py
@@ -0,0 +1,44 @@
+from twisted.internet import task
+from twisted.internet import reactor
+from Adafruit_BME280 import *
+import picamera
+
+timeout = 15.0
+"""
+def take_photo():
+
+    sensor = BME280(mode=BME280_OSAMPLE_8)   
+    
+    time = '{0:0.3f}'.format(sensor.t_fine)
+    photo_file_name = 'image' + time + '.jpg'
+
+    camera = picamera.PiCamera()
+    camera.capture(photo_file_name)
+"""
+
+def read_temp():
+
+    sensor = BME280(mode=BME280_OSAMPLE_8)
+
+    degrees = sensor.read_temperature()
+    pascals = sensor.read_pressure()
+    hectopascals = pascals / 100
+    humidity = sensor.read_humidity()
+    
+    timestamp = 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
+    temp = 'Temp = {0:0.3f} deg C'.format(degrees)
+    pressure = 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+    humidity = 'Humidity  = {0:0.2f} %'.format(humidity)
+
+    print timestamp
+    print temp
+    print pressure
+    print humidity
+    
+    return None
+
+
+l = task.LoopingCall(read_temp)
+l.start(timeout)
+
+reactor.run()
