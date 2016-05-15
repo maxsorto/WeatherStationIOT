00ac589a618daf357ba83053ce4267b80baa0936
maxsorto <maxsorto@gmail.com>
2016-05-15 05:32:36 -0400

    minor bug fixes

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import sys, time, bme
 print '\nRunning this script takes the current'
 print 'temperature, pressure and humidity reading every 5 minutes'
 print 'along with an image capture. \n'
-print "The data is stored in 'log.txt' and the images in the 'imgs' /n"
-print 'folder both found at the root of this script.'
+print "The data is stored in 'log.txt' and"
+print "the images in the 'imgs' folder both found at the root of this script."
 print '\n'
 print 'NOTE: Press ctrl+c to exit program!'
 print '\n'
